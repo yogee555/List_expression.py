76164880151960a908fbee942f3caef3a88402ad
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-13 10:37:35 +0530

    Add files via upload

---
+++ List_expression.py
@@ -0,0 +1,20 @@
+# check the length of list
+ch = [1,2,3]
+n=len([1,2,3])
+print(n)
+
+# list concatenation
+co = [1,2,3]+[4,5,6]
+print(co)
+
+# Repetation
+re = ['hi']*4
+print(re)
+
+# Membership
+Me = 3 in [1,2,3]
+print(Me)
+
+# Iteration
+for x in [1,2,3]:
+    print(x)
